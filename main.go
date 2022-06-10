e7607b7b8f41cd5f06e49ef79d76244b427917b9
Hiro <103487904+goferHiro@users.noreply.github.com>
2022-06-10 09:05:22 +0000

    Basic Auth

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"io"
+	"net/http"
 	"os"
 	"telecommunication/handlers"
 	"telecommunication/middlewares"
@@ -23,11 +24,20 @@ func main() {
 	router.Use(gin.Recovery(), middlewares.Logger(),
 		gindump.Dump())
 
+	routesWithBasicAuth := router.Group("/basicauth")
+	routesWithBasicAuth.Use(middlewares.BasicAuth())
+
 	// router.Use(gin.Logger())
 
 	router.GET("/", handlers.Home)
 	router.GET("/dummy", handlers.Dummy)
 	router.POST("/login", handlers.Login)
+
+	routesWithBasicAuth.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"success": true,
+		})
+	})
 
 	router.Run(":8080")
 }
